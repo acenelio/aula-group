01abbf6e087c4f1dd1c6e83c2cc526d9cda89376
Nelio Alves <acenelio@gmail.com>
2021-04-26 18:52:19 -0300

    Initial commit

---
+++ src/main/java/com/devsuperior/dsquiz/DsquizApplication.java
@@ -0,0 +1,12 @@
+package com.devsuperior.dsquiz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsquizApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsquizApplication.class, args);
+	}
+}
---
+++ src/test/java/com/devsuperior/dsquiz/DsquizApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dsquiz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsquizApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
